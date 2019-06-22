d1bf88f3369b8fb1e6b94b5ef711694325b2ccee
envaleed <jmarkanthony.062@gmail.com>
2019-06-23 00:36:43 +0800

    Android completed

---
+++ Android Development/Week 5/User and Tasks/app/src/main/java/volley/Config_URL.java
@@ -0,0 +1,11 @@
+package volley;
+
+//This class is for storing all URLs as a model of URLs
+
+public class Config_URL
+{
+	private static String base_URL = "http://192.168.3.22:8008/";
+	public static String URL_LOGIN = base_URL+"todo/api/v1.0/users";
+	public static String URL_REGISTER = base_URL+"android_login_api/";
+	public static String URL_TASKS = base_URL+"todo/api/v1.0/tasks";
+}
